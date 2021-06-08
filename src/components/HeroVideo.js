b828b03b1546e0530534e59273c6e595aefbfb6e
Anica Sutic <a.sutic@emakina.nl>
2021-06-08 12:33:30 +0200

    Fix hero vimeo video

--- src/components/HeroVideo.js
+++ src/components/HeroVideo.js
@@ -1,11 +1,30 @@
 'use strict';
 
 import React from 'react';
+import { matchMediaDown } from '../helpers/MediaQueries';
 
 const HeroVideo = () => {  
+
+  let responsiveStyles = {};
+
+	if (matchMediaDown(1280)) {
+		responsiveStyles.video = {
+			height:850
+		}
+	};
+	if (matchMediaDown(800)) {
+		responsiveStyles.video = {
+			height:300
+		}
+	};
+	if (matchMediaDown(550)) {
+		responsiveStyles.video = {
+			height:220
+		}
+	};
     return (
-      <div style={styles.container}>
-        <video src="https://tsg.sitesstage.com/cdn-media/tsg-content.mp4" style={styles.video} loop autoPlay muted controls={true} />
+      <div className="vimeo" style={styles.container}>
+        <iframe src="https://player.vimeo.com/video/559987734?autoplay=1&muted=1" style={{...styles.video, ...responsiveStyles.video}} frameborder="0"  allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
     );
   };
@@ -23,8 +42,8 @@ const styles = {
     video: {
         alignSelf: 'center',
         width: "100%",
-        height: "auto",
-        maxHeight: 750,
+        height:850,
+        // maxHeight: 750,
         resizeMode: 'cover',
         objectFit: 'cover'
     }
